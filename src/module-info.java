ed987e999605d53c86ef933e82ecfb422d000aa6
João Pedro <joao-pd13@hotmail.com>
2024-04-17 18:08:26 -0300

    Main View Controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
 module crud_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
+	requires javafx.media;
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
 }
+
+
+
